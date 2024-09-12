eae1e73a7c0f82d770030060804105ecb0ce98f5
KirillFI08 <kirillfischer08@gmail.com>
2024-09-12 15:31:17 +0200

    12.09

--- Vertex.java
+++ Vertex.java
@@ -11,15 +11,28 @@ public Vertex(double x, double y){
 
   Vertex v1 = new Vertex(10, 17);
   Vertex v2 = new Vertex(10, 16); 
+  Vertex v3 = v1.skalarMult(2);
 
   System.out.println(v1.length());
   System.out.println(v2.length());
+  System.out.println(v3);
  }
 
  public double length(){
    return Math.sqrt(x*x + y*y);
 
  }
+
+ public Vertex skalarMult(double s){
+   return new Vertex(x*s, y*s);
+
+ }
+
+
+public void skalarMultMod(double s){
+  x=x*s;
+  y=y*s;
+}
 
  public String toString(){
     return "("+x+","+y+")";
