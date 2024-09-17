f6ce9df1eca65d1d3b3db5ce31ddc0ef24835f8d
KirillFI08 <kirillfischer08@gmail.com>
2024-09-17 08:56:08 +0200

    Testklasse

--- Vertex.java
+++ Vertex.java
@@ -1,64 +1,61 @@
-class Vertex{
-    public double x;
-    public double y;
+class Vertex {
+  public double x;
+  public double y;
 
-public Vertex(double x, double y){
-    this.x=x;
-    this.y=y;
-}
-
- public static void main(String[] args) {
-
-  Vertex v1 = new Vertex(10, 17);
-  Vertex v2 = new Vertex(10, 16); 
-  Vertex v3 = v1.skalarMult(2);
-
-  System.out.println(v1.length());
-  System.out.println(v2.length());
-  System.out.println(v3);
- }
-
- public double length(){
-   return Math.sqrt(x*x + y*y);
-
- }
-
- public Vertex skalarMult(double s){
-   return new Vertex(x*s, y*s);
-
- }
-
-
-public void skalarMultMod(double s){
-  x=x*s; 
-  y=y*s;
-}
-
-public Vertex add(Vertex v2){
-  return new Vertex(x + v2.x, y + v2.y)
-}
-
-public Void addMod(Vertex v2){
-  x+=v2.x; //x=x+v2.x;  (langform)
-  y+=v2.y;
-}
-
-public void setX(double x){this.x = x;}
-public void setY(double y){this.y = y;}
-public double getX(){return x;}
-public double getY(){return y;}
-
- public String toString(){
-    return "("+x+","+y+")";
-
- }
-
-public boolean equals(Object thatObject){
-  if(thatObject instanceof Vertex){
-    that Vertex = (Vertex) thatObject;
-    return this.x == thatObject.x && this.y = thatObject.y;
+  public Vertex(double x, double y) {
+      this.x = x;
+      this.y = y;
   }
-  return false;
-}
 
+  public static void main(String[] args) {
+      Vertex v1 = new Vertex(10, 17);
+      Vertex v2 = new Vertex(10, 16); 
+      Vertex v3 = v1.skalarMult(2);
+      System.out.println(v1);
+      System.out.println(v1);
+      System.out.println(v1.length());
+      System.out.println(v2.length());
+      System.out.println(v3);
+      System.out.println(v1.equals(v3));
+      System.out.println(v1.equals(v1));
+  }
+
+  public double length() {
+      return Math.sqrt(x * x + y * y);
+  }
+
+  public Vertex skalarMult(double s) {
+      return new Vertex(x * s, y * s);
+  }
+
+  public void skalarMultMod(double s) {
+      x = x * s; 
+      y = y * s;
+  }
+
+  public Vertex add(Vertex v2) {
+      return new Vertex(x + v2.x, y + v2.y);
+  }
+
+  public void addMod(Vertex v2) {
+      x += v2.x; 
+      y += v2.y;
+  }
+
+  public void setX(double x) { this.x = x; }
+  public void setY(double y) { this.y = y; }
+  public double getX() { return x; }
+  public double getY() { return y; }
+
+  public String toString() {
+      return "(" + x + "," + y + ")";
+  }
+
+  public boolean equals(Object thatObject) {
+      if (thatObject instanceof Vertex) {
+          Vertex that = (Vertex) thatObject;
+          return this.x == that.x && this.y == that.y;
+      }
+      return false;
+  }
 }
