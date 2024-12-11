0658ee9447796ec2ad7a663ea15c1c5b0197bfdc
KirillFI08 <kirillfischer08@gmail.com>
2024-12-11 09:09:51 +0100

    11.12

---
+++ neu/Prime.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Prime {
+
+    public static void main(String[] args) {
+
+        System.out.println(output(generatePrimes(1000)));
+    }
+
+    public static boolean isPrime(int candidate) {
+        for (int divider = 2; divider <= Math.sqrt(candidate); divider++) {
+            if (candidate % divider == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static List<Integer> generatePrimes(int max) {
+        List<Integer> primes = new ArrayList<>();
+        for (int candidate = 2; candidate <= max; candidate++) {
+            if (isPrime(candidate)) {
+                primes.add(candidate);
+            }
+        }
+        return primes;
+
+    }
+
+    public static String output(List<Integer> primes) {
+        int counter = 0;
+        String output = "";
+        for (Integer prime : primes) {
+            output = output + " " + prime;
+            counter++;
+
+        }
+        output= output+"\nMenge: " + counter;
+        return output;
+    }
+
+
+}
+
+
